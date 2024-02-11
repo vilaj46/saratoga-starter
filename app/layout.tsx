6a5532dc571a875dd08327e519fe16c62a143def
Julian <vilaj46@gmail.com>
2024-02-10 20:45:16 -0500

    fixed imports for app director

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 'use client'
 
-import Footer from '@/components/Footer'
-import Header from '@/components/Header'
-import ScrollToTop from '@/components/ScrollToTop'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+import ScrollToTop from '../components/ScrollToTop'
 import { Inter } from 'next/font/google'
 import 'node_modules/react-modal-video/css/modal-video.css'
 import '../styles/globals.css'

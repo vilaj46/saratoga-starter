74c42dd151a5c7e70c038d8bc08db4da714fbd92
Julian <vilaj46@gmail.com>
2024-02-10 21:52:56 -0500

    Nav menu partial setup

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,18 +1,22 @@
 'use client'
+import { useEffect, useState } from 'react'
+
+import _ from '../../constants/en-us.json'
 
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import { useEffect, useState } from 'react'
-import ThemeToggler from './ThemeToggler'
+
+import Hamburger from './Hamburger'
+import { logo, logo_dark } from '../../constants/images'
 import menuData from './menuData'
+import routes from '../../constants/routes'
+import ThemeToggler from './ThemeToggler'
 
 const Header = () => {
 	// Navbar toggle
 	const [navbarOpen, setNavbarOpen] = useState(false)
-	const navbarToggleHandler = () => {
-		setNavbarOpen(!navbarOpen)
-	}
+	const navbarToggleHandler = () => setNavbarOpen(!navbarOpen)
 
 	// Sticky Navbar
 	const [sticky, setSticky] = useState(false)
@@ -39,75 +43,54 @@ const Header = () => {
 
 	const usePathName = usePathname()
 
+	const headerStickyClassName = sticky
+		? 'dark:bg-gray-dark dark:shadow-sticky-dark fixed z-[9999] bg-white !bg-opacity-80 shadow-sticky backdrop-blur-sm transition'
+		: 'absolute bg-transparent'
+	const logoWrapperClassName = sticky ? 'py-5 lg:py-2' : 'py-8'
+	const navbarClassName = navbarOpen
+		? 'visibility top-full opacity-100'
+		: 'invisible top-[120%] opacity-0'
+
+	const logoHeight = 30
+	const logoWidth = 140
+
 	return (
 		<>
 			<header
-				className={`header left-0 top-0 z-40 flex w-full items-center ${
-					sticky
-						? 'dark:bg-gray-dark dark:shadow-sticky-dark fixed z-[9999] bg-white !bg-opacity-80 shadow-sticky backdrop-blur-sm transition'
-						: 'absolute bg-transparent'
-				}`}
+				className={`flex header items-center left-0 top-0 w-full z-40 ${headerStickyClassName}`}
 			>
 				<div className='container'>
 					<div className='relative -mx-4 flex items-center justify-between'>
 						<div className='w-60 max-w-full px-4 xl:mr-12'>
 							<Link
-								href='/'
-								className={`header-logo block w-full ${
-									sticky ? 'py-5 lg:py-2' : 'py-8'
-								} `}
+								className={`block header-logo w-full ${logoWrapperClassName}`}
+								href={routes.home}
 							>
 								<Image
-									src='/images/logo/logo-2.svg'
 									alt='logo'
-									width={140}
-									height={30}
 									className='w-full dark:hidden'
+									height={logoHeight}
+									src={logo}
+									width={logoWidth}
 								/>
 								<Image
-									src='/images/logo/logo.svg'
 									alt='logo'
-									width={140}
-									height={30}
 									className='hidden w-full dark:block'
+									height={logoHeight}
+									src={logo_dark}
+									width={logoWidth}
 								/>
 							</Link>
 						</div>
 						<div className='flex w-full items-center justify-between px-4'>
 							<div>
-								<button
-									onClick={navbarToggleHandler}
-									id='navbarToggler'
-									aria-label='Mobile Menu'
-									className='absolute right-4 top-1/2 block translate-y-[-50%] rounded-lg px-3 py-[6px] ring-primary focus:ring-2 lg:hidden'
-								>
-									<span
-										className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300 dark:bg-white ${
-											navbarOpen
-												? ' top-[7px] rotate-45'
-												: ' '
-										}`}
-									/>
-									<span
-										className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300 dark:bg-white ${
-											navbarOpen ? 'opacity-0 ' : ' '
-										}`}
-									/>
-									<span
-										className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300 dark:bg-white ${
-											navbarOpen
-												? ' top-[-8px] -rotate-45'
-												: ' '
-										}`}
-									/>
-								</button>
+								<Hamburger
+									isOpen={navbarOpen}
+									onToggle={navbarToggleHandler}
+								/>
 								<nav
+									className={`navbar absolute right-0 z-30 w-[250px] rounded border-[.5px] border-body-color/50 bg-white px-6 py-4 duration-300 dark:border-body-color/20 dark:bg-dark lg:visible lg:static lg:w-auto lg:border-none lg:!bg-transparent lg:p-0 lg:opacity-100 ${navbarClassName}`}
 									id='navbarCollapse'
-									className={`navbar absolute right-0 z-30 w-[250px] rounded border-[.5px] border-body-color/50 bg-white px-6 py-4 duration-300 dark:border-body-color/20 dark:bg-dark lg:visible lg:static lg:w-auto lg:border-none lg:!bg-transparent lg:p-0 lg:opacity-100 ${
-										navbarOpen
-											? 'visibility top-full opacity-100'
-											: 'invisible top-[120%] opacity-0'
-									}`}
 								>
 									<ul className='block lg:flex lg:space-x-12'>
 										{menuData.map((menuItem, index) => (
@@ -191,17 +174,17 @@ const Header = () => {
 								</nav>
 							</div>
 							<div className='flex items-center justify-end pr-16 lg:pr-0'>
-								<Link
+								{/* <Link
 									href='/signin'
 									className='hidden px-7 py-3 text-base font-medium text-dark hover:opacity-70 dark:text-white md:block'
 								>
-									Sign In
-								</Link>
+									TODO: What should be here?
+								</Link> */}
 								<Link
-									href='/signup'
-									className='ease-in-up shadow-btn hover:shadow-btn-hover hidden rounded-sm bg-primary px-8 py-3 text-base font-medium text-white transition duration-300 hover:bg-opacity-90 md:block md:px-9 lg:px-6 xl:px-9'
+									className='uppercase ease-in-up shadow-btn hover:shadow-btn-hover hidden rounded-sm bg-primary px-8 py-3 text-base font-medium text-white transition duration-300 hover:bg-opacity-90 md:block md:px-9 lg:px-6 xl:px-9'
+									href={routes.resNexus}
 								>
-									Sign Up
+									{_.BookNow}
 								</Link>
 								<div>
 									<ThemeToggler />
---
+++ constants/routes.ts
@@ -0,0 +1,8 @@
+export default {
+  giftCertificates: '/gift-certificates',
+  explore: '/explore',
+  home: '/',
+  lodging: '/lodging',
+  resNexus: 'https://resnexus.com/resnexus/reservations/book/5888F13B-7306-4082-89C2-B6F42CC9CD92/?',
+  saratogaSprings: '/saratoga-springs'
+}

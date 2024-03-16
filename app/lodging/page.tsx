91bde6814ba6f6c18435b2de8c5554b65911daac
Julian <vilaj46@gmail.com>
2024-03-16 14:20:36 -0400

    rooms error

--- app/lodging/page.tsx
+++ app/lodging/page.tsx
@@ -18,10 +18,10 @@ const RoomsPage = () => {
 				pageName='Guest Rooms'
 				description='Brunswick Bed and Breakfast is proud to offer uniquely decorated and luxuriously appointed guest rooms. The distinctive Saratoga Springs lodging of our B&B inn is sure to delight you and make your stay in the area that much more special and memorable.'
 			/>
-			<Rooms
+			{/* <Rooms
 				description='One of three rooms located on the first floor, Room A is ideal for long or short term stays. This unique two-room suite features a queen bed and spacious sitting areas. Original stained glass windows adorn the front room sitting area. The room adjacent offers a single sleigh bed and additional area to relax. All rooms offer central AC/heat and feature private bath with shower, custom amenities, hairdryers, 32" flat screen TVs, free Wi-Fi, complementary bottles of Saratoga Water.'
 				heading={'Explore Our Saratoga Springs Lodging Room A'}
-			/>
+			/> */}
 		</>
 	)
 }
